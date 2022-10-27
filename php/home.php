611048b28b489a3cdac790dc8d94d1578dde2976
Rarrus <52527380+Rarrus@users.noreply.github.com>
2022-10-27 10:37:08 +0200

    font

--- php/home.php
+++ php/home.php
@@ -20,6 +20,7 @@ require_once './config/config.php'; // ajout connexion bdd
   <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="./../js/screen.js"></script>
+  <link href=’https://fonts.googleapis.com/css?family=Fuzzy+Bubbles’ rel=’stylesheet’/>
 
 </head>
 
